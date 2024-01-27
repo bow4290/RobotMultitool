20e13bb8bedee90c986cbf2306ec109a2838d61d
JumpingPistachio <JumpingPistachio@gmail.com>
2024-01-26 19:42:24 -0500

    Add SendableChoosers for selecting motor types

--- src/main/java/frc/robot/subsystems/MotorMan.java
+++ src/main/java/frc/robot/subsystems/MotorMan.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.lib.GenericMotors.BlankGenericMotor;
 import frc.robot.lib.GenericMotors.GenericMotor;
@@ -13,8 +15,10 @@ import frc.robot.lib.GenericMotors.GenericTalonFX;
  */
 public class MotorMan extends SubsystemBase{
 
+  SendableChooser<MotorEnum> _chooserTemplate = new SendableChooser<>();
+  
   GenericMotor motor1, motor2, motor3, motor4;
-  MotorEnum enumMotor1, enumMotor2, enumMotor3, enumMotor4;
+  SendableChooser<MotorEnum> chooserTypeMotor1, chooserTypeMotor2, chooserTypeMotor3, chooserTypeMotor4;
   
 
   public enum MotorEnum{
@@ -24,28 +28,23 @@ public class MotorMan extends SubsystemBase{
   }
 
   /**
-   * The helper function for handling setting motor type, based on ID. This is not meant to be called directly, instead use {@link #motorSwitch(int, MotorEnum) motorSwitch()}
+   * The helper function for handling setting motor type, based on ID. This is not meant to be called directly, instead use {@link #setMotorType(int, MotorEnum) motorSwitch()}
    * @param motorID - The ID of the motor that should be changed. Value should be of type int between 1-4.
    * @param genericMotorType - The GenericMotor implementation to set. Value should be a new GenericMotor implementation instance.
-   * @param enumMotorType - The value that should be set for that motors instance of MotorEnum. Used to handle state.
    */
-  private void _motorIDHandler(int motorID, GenericMotor genericMotorType, MotorEnum enumMotorType){
+  private void _motorIDHandler(int motorID, GenericMotor genericMotorType){
     switch (motorID) {
       case 1:
-        this.motor1 = genericMotorType;
-        this.enumMotor1 = enumMotorType;
+        motor1 = genericMotorType;
         break;
       case 2:
-        this.motor2 = genericMotorType;
-        this.enumMotor2 = enumMotorType;
+        motor2 = genericMotorType;
         break;
       case 3:
-        this.motor3 = genericMotorType;
-        this.enumMotor3 = enumMotorType;
+        motor3 = genericMotorType;
         break;
       case 4:
-        this.motor4 = genericMotorType;
-        this.enumMotor4 = enumMotorType;
+        motor4 = genericMotorType;
         break;
 
       default:
@@ -55,18 +54,24 @@ public class MotorMan extends SubsystemBase{
 
   }
 
-  public void motorSwitch(int motorID, MotorEnum motorTypeEnum){
+  /**
+   * Sets the motor type for a specific GenericMotor instance, based on its ID and the MotorEnum value to be switched to. In most cases, {@link #refreshMotorTypes()} should be used instead.
+   * 
+   * @param motorID - The ID of the motor to set, of value int.
+   * @param motorTypeEnum - The type of motor to change to, of value MotorEnum.
+   */
+  public void setMotorType(int motorID, MotorEnum motorTypeEnum){
     switch (motorTypeEnum) {
       case None:
-        _motorIDHandler(motorID, new BlankGenericMotor(motorID), MotorEnum.None);
+        _motorIDHandler(motorID, new BlankGenericMotor(motorID));
         break;
       
       case TalonFX:
-        _motorIDHandler(motorID, new GenericTalonFX(motorID), MotorEnum.TalonFX);
+        _motorIDHandler(motorID, new GenericTalonFX(motorID));
         break;
 
       case SparkMax:
-        _motorIDHandler(motorID, new GenericSparkMax(motorID, MotorType.kBrushless), MotorEnum.SparkMax);
+        _motorIDHandler(motorID, new GenericSparkMax(motorID, MotorType.kBrushless));
         break;
 
       default:
@@ -74,9 +79,43 @@ public class MotorMan extends SubsystemBase{
     }
   }
 
+  /**
+   * Sets up all the SendableChooser instances. Should be ran once at robot initialization.
+   */
+  public void chooserConfig(){
+    _chooserTemplate.setDefaultOption("None", MotorEnum.None);
+    _chooserTemplate.addOption("TalonFX", MotorEnum.TalonFX);
+    _chooserTemplate.addOption("SparkMax", MotorEnum.SparkMax);
+
+    chooserTypeMotor1 = _chooserTemplate;
+    chooserTypeMotor2 = _chooserTemplate;
+    chooserTypeMotor3 = _chooserTemplate;
+    chooserTypeMotor4 = _chooserTemplate;
+
+    SmartDashboard.putData("Motor 1 Type", chooserTypeMotor1);
+    SmartDashboard.putData("Motor 2 Type", chooserTypeMotor2);
+    SmartDashboard.putData("Motor 3 Type", chooserTypeMotor3);
+    SmartDashboard.putData("Motor 4 Type", chooserTypeMotor4);
+  }
+  
+  /**
+   * Automatically sets all motors to the types selected by their individual SendableChooser instances.
+   */
+  public void refreshMotorTypes(){
+    setMotorType(1, chooserTypeMotor1.getSelected());
+    setMotorType(2, chooserTypeMotor2.getSelected());
+    setMotorType(3, chooserTypeMotor3.getSelected());
+    setMotorType(4, chooserTypeMotor4.getSelected());
+  }
+
+
 
   @Override
   public void periodic(){
-
+    // - SmartDashboard -
+    SmartDashboard.putNumber("Motor 1 Current Speed", motor1.getSpeed());
+    SmartDashboard.putNumber("Motor 2 Current Speed", motor2.getSpeed());
+    SmartDashboard.putNumber("Motor 3 Current Speed", motor3.getSpeed());
+    SmartDashboard.putNumber("Motor 4 Current Speed", motor4.getSpeed());
   }
 }
